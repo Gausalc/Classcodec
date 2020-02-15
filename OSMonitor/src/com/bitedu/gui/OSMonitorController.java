1b015bd14a14f87d319328fb2cfbf275990eda4a
Gausalc <1178321407@qq.com>
2020-02-15 14:15:55 +0800

    11

--- OSMonitor/src/com/bitedu/gui/OSMonitorController.java
+++ OSMonitor/src/com/bitedu/gui/OSMonitorController.java
@@ -1,4 +1,15 @@
 package com.bitedu.gui;
 
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+
 public class OSMonitorController {
+    public void handleCPUSelectionChanged(Event event) {
+        System.out.println();
+    }
+
+
+    public void handleSelectFile(ActionEvent actionEvent) {
+        System.out.println();
+    }
 }
