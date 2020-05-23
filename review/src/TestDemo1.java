7a9d4b03078e7e199260fecf2686c2f1ee4c3037
Gausalc <1178321407@qq.com>
2020-05-23 12:41:21 +0800

    1

---
+++ review/src/TestDemo1.java
@@ -0,0 +1,11 @@
+public class TestDemo1 {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = a++;
+        System.out.println(b);
+        System.out.println(a);
+//        int c = ++a;
+//        System.out.println(c);
+//        System.out.println(a);
+    }
+}
