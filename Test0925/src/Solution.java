35a2c00328b9081f00cfb1e68a516ea21db68392
Gausalc <1178321407@qq.com>
2020-09-25 23:50:11 +0800

    1

---
+++ Test0925/src/Solution.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Solution {
+    /**
+     * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+     * 返回Interval类，start代表汪仔最少做对了多少道题，end代表汪仔最多做对了多少道题。
+     * @param n int整型 选择题总数
+     * @param k int整型 朋友做对的题数
+     * @param str1 string字符串 长度为n只包含ABCD的字符串，其中第i个代表汪仔第i题做出的选择
+     * @param str2 string字符串 长度为n只包含ABCD的字符串，其中第i个代表朋友第i题做出的选择
+     * @return Interval类
+     */
+    public Interval solve (int n, int k, String str1, String str2) {
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        String str1 = sc.next();
+        String str2 = sc.next();
+        System.out.println();
+    }
+}
